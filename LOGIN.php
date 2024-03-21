bf6337826e7f685ed250cb42d1d4e81dbc2b5ff5
NicoleWachira <146592972+NicoleWachira@users.noreply.github.com>
2024-03-21 17:05:43 +0300

    file changes

--- LOGIN.php
+++ LOGIN.php
@@ -20,7 +20,7 @@ if ($result) {
       session_start();//start user session
       $_SESSION['email']=$email;
       $_SESSION['id']=$shuka['id'];
-      header("location:home.html");
+      header("Location: about.html");
       exit();
     } else{
      // echo"Invalid login";
@@ -45,8 +45,69 @@ if ($result) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Login</title>
     <link rel="stylesheet" href="LOGIN.CSS">
+
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet"> 
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+
+
+
 </head>
 <body>
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-fluid position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
+                <a href="index.html" class="navbar-brand p-0">
+                    <h1 class="text-primary m-0"><i ><img src="img/logo.jpg" alt="Logo"></i>Shuka</h1>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="about.html" class="nav-item nav-link">Home</a>
+                        <a href="signup.php" class="nav-item nav-link">Signup</a>
+                        <a href="LOGIN.php" class="nav-item nav-link">Login</a>
+                        
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="appointment.html" class="dropdown-item">Appointment</a>
+                                <a href="feature.html" class="dropdown-item">Features</a>
+                                
+                                <a href="testimonial.html" class="dropdown-item">Reviews</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link active">Contact Us</a>
+                    </div>
+                    <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Navbar End -->
+
+
+
+
+
   <div class="wrapper">
     <div class="container main">
         <div class="row">
--- index.php
+++ index.php
@@ -14,32 +14,107 @@ if(isset($_POST['submit'])) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajaxlibs/font-awesome/6.2/css/all.min.css">
-    <link rel="stylesheet"href="signup.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>SIGNUP</title>
+   
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet"> 
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheets" href="signup.css">
   </head>
   <body>
-    <div id ="form">
-        <h1 id="heading">Signup Form </h1><br>
-    <form action="" method="POST"  >
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-</svg>
-        <input type="text" id="user" name="user" placeholder="Enter Username" required><br> <br>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-</svg>
-        <input type="email" id="email" name="email" placeholder="Enter email" required><br> <br>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-  <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2"/>
-</svg>
-        <input type="password" id="password" name="password" placeholder="Enter password" required><br> <br><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-      <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2"/>
-     </svg>
-        <input type="password" id="cpass" name="cpass" placeholder="Retype password" required><br> <br>
 
-    <input type="submit" id="btn" value="Signup"  name="submit"/>
+ <!-- Navbar & Hero Start -->
+ <div class="container-fluid position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
+                <a href="index.html" class="navbar-brand p-0">
+                    <h1 class="text-primary m-0"><i ><img src="img/logo.jpg" alt="Logo"></i>Shuka</h1>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="about.html" class="nav-item nav-link">Home</a>
+                        <a href="signup.php" class="nav-item nav-link">Signup</a>
+                        <a href="LOGIN.php" class="nav-item nav-link">Login</a>
+                        
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="appointment.html" class="dropdown-item">Appointment</a>
+                                <a href="feature.html" class="dropdown-item">Features</a>
+                                
+                                <a href="testimonial.html" class="dropdown-item">Reviews</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link active">Contact Us</a>
+                    </div>
+                    <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Navbar End -->
+
+
+
+<br>
+<br>
+
+<div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">
+                <div class="text">
+                    <p> SHUKA! <i>  Discover your perfect hairstyle with us  </i></p>
+                </div>     
+                <!-------------      image     ------------->
+                
+                
+            </div>
+            <div class="col-md-6 right">
+                
+                <div class="input-box">
+                   
+                   <header>Log into account</header>
+                   <form method="home.html" action="POST" target="home.html">
+                   <div class="input-field">
+                        <input type="text" class="input" id="email"name="email" required="" autocomplete="off">
+                        <label for="email">Email</label> 
+                    </div> 
+                   <div class="input-field">
+                        <input type="password" class="input" id="pass" name="password" required="">
+                        <label for="pass">Password</label>
+                    </div> 
+                   <div class="input-field">
+                        
+                        <input type="submit" class="submit" value="Login">
+                   </div> 
 </form>
+                   <div class="signup">
+                   <span>Don't have an account? <a href="index.php">Sign up here</a></span>
+                   </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</div>
   </body>
 </html>
