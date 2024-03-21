05b6d78680ed58bfa27282f66af96cf710d48d89
NicoleWachira <146592972+NicoleWachira@users.noreply.github.com>
2024-03-21 18:57:36 +0300

    file changes

--- LOGIN.php
+++ LOGIN.php
@@ -84,21 +84,21 @@ if ($result) {
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
                         <a href="about.html" class="nav-item nav-link">Home</a>
-                        <a href="signup.php" class="nav-item nav-link">Signup</a>
+                        <a href="index.php" class="nav-item nav-link">Signup</a>
                         <a href="LOGIN.php" class="nav-item nav-link">Login</a>
                         
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu m-0">
                                 <a href="appointment.html" class="dropdown-item">Appointment</a>
-                                <a href="feature.html" class="dropdown-item">Features</a>
+                                <a href="feature.html" class="dropdown-item">Hairstyles</a>
                                 
                                 <a href="testimonial.html" class="dropdown-item">Reviews</a>
                             </div>
                         </div>
                         <a href="contact.html" class="nav-item nav-link active">Contact Us</a>
                     </div>
-                    <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
+                    <a href="BookAppointment.html" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
                 </div>
             </nav>
         </div>
--- index.php
+++ index.php
@@ -9,17 +9,22 @@ if(isset($_POST['submit'])) {
   exit(); // Ensure script execution stops after redirection
 }
 ?>
-<!doctype html>
+
+
+
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>SIGNUP</title>
-   
-<!-- Google Web Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <head>
+        <meta charset="utf-8">
+        <title> signup </title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet"> 
 
@@ -37,35 +42,71 @@ if(isset($_POST['submit'])) {
 
         <!-- Template Stylesheet -->
         <link href="css/style.css" rel="stylesheet">
-        <link rel="stylesheets" href="signup.css">
-  </head>
-  <body>
+        <link href="css/BookAppointment.css" rel="stylesheet">
+        <script src="js/BookAppointment.js" defer></script>
+    </head>
 
- <!-- Navbar & Hero Start -->
- <div class="container-fluid position-relative p-0">
+    <body>
+
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Topbar Start -->
+        <div class="container-fluid bg-dark px-5 d-none d-lg-block">
+            <div class="row gx-0 align-items-center" style="height: 45px;">
+                <div class="col-lg-8 text-center text-lg-start mb-lg-0">
+                    <div class="d-flex flex-wrap">
+                        <a href="#" class="text-light me-4"><i class="fas fa-map-marker-alt text-primary me-2"></i>Nairobi</a>
+                        <a href="#" class="text-light me-4"><i class="fas fa-phone-alt text-primary me-2"></i>+254794422351</a>
+                        <a href="#" class="text-light me-0"><i class="fas fa-envelope text-primary me-2"></i>Shuka@gmail.com</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center text-lg-end">
+                    <div class="d-flex align-items-center justify-content-end">
+                        <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-snapchat"></i></a>
+                        <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-0"><i class="fab fa-tiktok"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Topbar End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-fluid position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
                 <a href="index.html" class="navbar-brand p-0">
-                    <h1 class="text-primary m-0"><i ><img src="img/logo.jpg" alt="Logo"></i>Shuka</h1>
+                    <h1 class="text-primary m-0"><i><img src="img/logo.jpg" alt="Logo"></i>Shuka</h1>
+                  
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
+                        
                         <a href="about.html" class="nav-item nav-link">Home</a>
-                        <a href="signup.php" class="nav-item nav-link">Signup</a>
-                        <a href="LOGIN.php" class="nav-item nav-link">Login</a>
+                        <a href="index.html" class="nav-item nav-link">Signup</a>
+                        <a href="Login.html" class="nav-item nav-link">Login</a>
                         
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu m-0">
-                                <a href="appointment.html" class="dropdown-item">Appointment</a>
-                                <a href="feature.html" class="dropdown-item">Features</a>
-                                
-                                <a href="testimonial.html" class="dropdown-item">Reviews</a>
+                                <a href="appointment.html" class="dropdown-item active">Appointment</a>
+                                <a href="feature.html" class="dropdown-item">Services</a>
+                                <a href="testimonial.html" class="dropdown-item">Testimonials</a>
+                               
                             </div>
                         </div>
-                        <a href="contact.html" class="nav-item nav-link active">Contact Us</a>
+                        <a href="contact.html" class="nav-item nav-link">Contact Us</a>
                     </div>
                     <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
                 </div>
@@ -74,47 +115,69 @@ if(isset($_POST['submit'])) {
         <!-- Navbar End -->
 
 
-
-<br>
-<br>
-
-<div class="wrapper">
-    <div class="container main">
-        <div class="row">
-            <div class="col-md-6 side-image">
-                <div class="text">
-                    <p> SHUKA! <i>  Discover your perfect hairstyle with us  </i></p>
-                </div>     
-                <!-------------      image     ------------->
-                
-                
-            </div>
-            <div class="col-md-6 right">
-                
-                <div class="input-box">
-                   
-                   <header>Log into account</header>
-                   <form method="home.html" action="POST" target="home.html">
-                   <div class="input-field">
-                        <input type="text" class="input" id="email"name="email" required="" autocomplete="off">
-                        <label for="email">Email</label> 
-                    </div> 
-                   <div class="input-field">
-                        <input type="password" class="input" id="pass" name="password" required="">
-                        <label for="pass">Password</label>
-                    </div> 
-                   <div class="input-field">
-                        
-                        <input type="submit" class="submit" value="Login">
-                   </div> 
-</form>
-                   <div class="signup">
-                   <span>Don't have an account? <a href="index.php">Sign up here</a></span>
-                   </div>
-                </div>  
+        <!-- Header Start -->
+        <div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style="max-width: 900px;">
+                <h3 class="text-white display-3 mb-4 wow fadeInDown" ="0.1s"><a href="BookAppointment.html"></a>Signup</h1>
+                <ol class="breadcrumb justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item active text-primary">Appointment</li>
+                </ol>    
             </div>
         </div>
-    </div>
-</div>
-  </body>
+        <!---start of form---->
+        <div class="appointment-container">
+            <h2>Signup</h2>
+            <form action="js/submit_booking" method="POST">
+              <label for="name">Enter Username:</label>
+              <input type="text" id="name" name="name" required>
+          
+              <label for="phone">Enter email:</label>
+              <input type="email" id="email" name="email" required>
+
+              <label for="password">Enter password:</label>
+              <input type="password" id="password" name="password" required>
+
+              <label for="confirmpassword">Confirm password:</label>
+              <input type="password" id="confirmpassword" name="confirmpassword" required>
+
+
+
+
+
+              <button type="submit"><a href="about.html">Submit</a></button>
+            </form>
+        </div>
+        <div class="container-fluid copyright py-4">
+            <div class="container">
+                <div class="row g-4 align-items-center">
+                    
+                    <div class="col-md-6 text-center text-md-end text-white">
+                        
+                        <a class="border-bottom" href="https://github.com/anastacia254/Shukaweb">Shuka founders</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-primary btn-lg-square back-to-top"><i class="fa fa-arrow-up"></i></a>   
+
+        
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        
+
+        
+        <script src="js/main.js"></script>
+        
+    </body>
+
 </html>
